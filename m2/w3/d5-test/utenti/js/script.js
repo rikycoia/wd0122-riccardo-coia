76f1d73c63049b3159cca09c293dfc877b21dc5e
rikycoia <riccardo.coia@outlook.it>
2022-05-13 16:52:19 +0200

    first commit

---
+++ m2/w3/d5-test/utenti/js/script.js
@@ -0,0 +1,35 @@
+const APPURL = 'utenti/json/users.json'
+
+let tabella = document.querySelector('#users')
+
+fetch(APPURL)
+.then(res => res.json())
+.then(res => {
+
+    for(let utente of res){
+        let tr = document.createElement('tr')
+    
+
+        let tdUsername = document.createElement('td')
+        tdUsername.innerHTML = utente.username
+       
+        let tdNome = document.createElement('td')
+        tdNome.innerHTML = utente.firstName
+
+        let tdCognome = document.createElement('td')
+        tdCognome.innerHTML = utente.lastName
+
+        let tdGender = document.createElement('td')
+        tdGender.innerHTML = utente.gender
+
+        let tdEmail = document.createElement('td')
+        tdEmail.innerHTML = utente.email
+
+        let tdImg1 = document.createElement('td')
+        tdImg1.innerHTML = `<img src=" ${utente.profileURL} " width=75px>`
+
+        tr.append(tdImg1,tdUsername,tdNome,tdCognome,tdGender,tdEmail)
+
+        tabella.append(tr)
+    }
+    })
