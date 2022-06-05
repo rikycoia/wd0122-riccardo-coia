76013ae8ea7d37a6dd8b425b69a2b74d6910ee75
rikycoia <riccardo.coia@outlook.it>
2022-06-05 18:29:25 +0200

    first commit

--- m3/w2/d5-test/todo/src/app/todo-completati/todo-completati.component.ts
+++ m3/w2/d5-test/todo/src/app/todo-completati/todo-completati.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { List } from '../class/todo';
+import { Todoservice } from '../service/todo.service';
+
 
 @Component({
   selector: 'app-todo-completati',
@@ -7,9 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoCompletatiComponent implements OnInit {
 
-  constructor() { }
+  constructor( private todolist: Todoservice) { }
+
+
+  arrayCompletati!: List[];
+
+  id: number = 1;
+  title:string = "";
+  completed= false;
 
   ngOnInit(): void {
-  }
 
+    let c = this.completed
+      this.completed = true;
+
+      this.arrayCompletati.filter(c => this.completed == true)
+
+  }
 }
+
+
+
+
--- m3/w2/d5-test/todo/src/app/todo/todo.component.ts
+++ m3/w2/d5-test/todo/src/app/todo/todo.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { List } from '../class/todo';
 import { Todoservice } from '../service/todo.service';
 
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -13,38 +14,47 @@ export class TodoComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  titolo: string | undefined;
+
   constructor(private todolist: Todoservice ){}
 
   input:string = ""
+  selected:boolean = true;
 
   id:number = 1
   title:string = ""
-  completed = false
+  completed = true
+  active: boolean = true
 
   todo:List[] = [
     {
      id: 1,
      title: 'comprare i biglietti',
-     completed: false
+     completed: true
     }
    ];
 
-  arrayCompletati!: List[];
+  arrayCompletati:List[] = []
 
 
+ completa():void{
+   this.completed = true;
 
+   this.arrayCompletati = this.todo.filter( (e: { completed: boolean; }) => e.completed == true);
+ }
 
-  aggiungi(){
+aggiungi(){
     let titolo = this.input
     let nuovaLista = new List(this.id, titolo, this.completed);
     this.todo.push(nuovaLista)
   }
 
-  completa(){
-    let c = this.completed
-    this.completed = true;
+  nascondi(){
+    this.active = !this.active
+  }
 
-    this.arrayCompletati.filter(c => this.completed == true)}
+  removeTodo(){
+    if (this.selected == true) {
 
+    }
+  }
 }
